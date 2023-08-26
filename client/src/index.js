6872c1f65ccf2ea04709c0fdc80c8cafcf29becf
PRESSRAIN-LEE <pressrainlee@gmail.com>
2023-08-26 15:32:16 +0900

    useEffect 두 번 실행 방지

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>   //두번 실행되는것 방지
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>  //두번 실행되는것 방지
 );

30c767357bc0adabec8fe02d995b09a640836436
PRESSRAIN-LEE <pressrainlee@gmail.com>
2023-08-26 15:35:44 +0900

    useEffect 두 번 실행 방지

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>   //두번 실행되는것 방지
+  // <React.StrictMode>   //두번 실행되는것 방지  //(https://youngble.tistory.com/175)
     <App />
   // </React.StrictMode>  //두번 실행되는것 방지
 );

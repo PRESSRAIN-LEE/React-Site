62b39fefe0fab6324cf95f29b1078d698efd07a1
PRESSRAIN-LEE <pressrainlee@gmail.com>
2023-08-29 23:02:04 +0900

    회원가입

--- client/src/pages/login/Join.js
+++ client/src/pages/login/Join.js
@@ -1,27 +1,38 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 import Form from "react-bootstrap/Form"; 
 import Button from "react-bootstrap/Button";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 //https://gom20.tistory.com/158
 
 function Join(){
+    const navigate = useNavigate();
+
     const [userId, setUserId] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+    const [userPw, setUserPw] = useState("");
+    const [userPwChk, setUserPwChk] = useState("");
     const [userName, setUserName] = useState("");
-    const [email, setEmail] = useState("");
+    const [userMail, setUserMail] = useState("");
 
     const [userIdError, setUserIdError] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
     const [confirmPasswordError, setConfirmPasswordError] = useState(false);
     const [userNameError, setUserNameError] = useState(false);
     const [emailError, setEmailError] = useState(false);
+
+    // const [data, setData] = useState({
+	// 	userId: '',
+	// 	password: '',
+    //     confirmPassword: '',
+	// 	userName: '',
+	// 	email: '',
+	// });
 
     const onChangeUserId = (e) => {
         const userIdRegex = /^[A-Za-z0-9+]{5,}$/;
@@ -35,15 +46,15 @@ function Join(){
         if ((!e.target.value || (passwordRegex.test(e.target.value)))) setPasswordError(false);
         else setPasswordError(true);
 
-        if (!confirmPassword || e.target.value === confirmPassword) setConfirmPasswordError(false);
+        if (!userPwChk || e.target.value === userPwChk) setConfirmPasswordError(false);
         else setConfirmPasswordError(true);
-        setPassword(e.target.value);
+        setUserPw(e.target.value);
     };
 
     const onChangeConfirmPassword = (e) => {
-        if (password === e.target.value) setConfirmPasswordError(false);
+        if (userPw === e.target.value) setConfirmPasswordError(false);
         else setConfirmPasswordError(true);
-        setConfirmPassword(e.target.value);
+        setUserPwChk(e.target.value);
     };
 
     const onChangeUserName = (e) => {
@@ -55,18 +66,18 @@ function Join(){
         const emailRegex = /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
         if (!e.target.value || emailRegex.test(e.target.value)) setEmailError(false);
         else setEmailError(true);
-        setEmail(e.target.value);
+        setUserMail(e.target.value);
     };
 
     const validation = () => {
         if(!userId) setUserIdError(true);
-        if(!password) setPasswordError(true);
-        if(!confirmPassword) setConfirmPasswordError(true);
+        if(!userPw) setPasswordError(true);
+        if(!userPwChk) setConfirmPasswordError(true);
         if(!userName) setUserNameError(true);
-        if(!email) setEmailError(true);
+        if(!userMail) setEmailError(true);
 
-        if(userId && password && confirmPassword && userName && email) return true;
-        else return false;
+        if(userId && userPw && userPwChk && userName && userMail) return false;
+        else return true;
     }
 
     const onSubmit = (e) => {
@@ -82,14 +93,27 @@ function Join(){
             return setEmailError(true);
         }
         */
-        console.log({
-            userId,
-            userName,
-            password,
-            confirmPassword,
-            email
-        });
+        // console.log("userId_+_: ", userId);
+        // console.log({
+        //     userId,
+        //     userName,
+        //     password,
+        //     confirmPassword,
+        //     email
+        // });
 
+        axios.post("/api/members/save", {userId, userName, userPw, userMail})
+		.then(res => {
+			console.log(res.data);
+			if(res.data.Message === "ERROR"){
+				alert("오류 발생");
+			}else if(res.data.Message === "VALIDATE"){
+                alert("미입력 발생");
+			}else{
+				navigate('/');
+			}
+		})
+		.catch(err => console.log(err));
     }
 
     return (
@@ -97,43 +121,43 @@ function Join(){
             <div>
                 <Container className="panel">
                     <Form>
-                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextId">
+                        <Form.Group as={ Row } className="mb-3" controlId="formPlaintextId">
                             <Col sm>
-                                <Form.Control maxLength={20} placeholder="UserID" value={userId} onChange={onChangeUserId} />
-                                {userIdError && <div className="invalid-input">User ID must be at least 5 letter and contain letters or numbers.</div>}
+                                <Form.Control maxLength={ 20 } placeholder="UserID" name="userID" value={ userId } onChange={onChangeUserId} />
+                                { userIdError && <div className="invalid-input">User ID must be at least 5 letter and contain letters or numbers.</div> }
                             </Col>
                         </Form.Group>
 
-                        <Form.Group as={Row} className="mb-3">
+                        <Form.Group as={ Row } className="mb-3">
                             <Col sm>
-                                <Form.Control maxLength={20} type="password" placeholder="Password" value={password} onChange={onChangePassword} />
-                                {passwordError && <div className="invalid-input">Password must be at least 8 characters and contain at least one letter and one number. </div>}
+                                <Form.Control maxLength={ 20 } type="password" placeholder="Password" name="userPw" value={ userPw } onChange={onChangePassword} />
+                                { passwordError && <div className="invalid-input">Password must be at least 8 characters and contain at least one letter and one number. </div> }
                             </Col>
                         </Form.Group>
 
-                        <Form.Group as={Row} className="mb-3">
+                        <Form.Group as={ Row } className="mb-3">
                             <Col sm>
-                                <Form.Control maxLength={20} type="password" placeholder="Confirm Password" value={confirmPassword} onChange={onChangeConfirmPassword} />
-                                {confirmPasswordError && <div className="invalid-input">Those passwords didn't match.</div>}
+                                <Form.Control maxLength={ 20 } type="password" placeholder="Confirm Password" name="userPwChk" value={ userPwChk } onChange={ onChangeConfirmPassword } />
+                                { confirmPasswordError && <div className="invalid-input">Those passwords didn't match.</div> }
                             </Col>
                         </Form.Group>
 
-                        <Form.Group as={Row} className="mb-3">
+                        <Form.Group as={ Row } className="mb-3">
                             <Col sm>
-                                <Form.Control maxLength={20} placeholder="Username" value={userName} onChange={onChangeUserName} />
+                                <Form.Control maxLength={ 20 } placeholder="UserName" name="userName" value={ userName } onChange={ onChangeUserName } />
                                 {userNameError && <div className="invalid-input">Required.</div>}
                             </Col>
                         </Form.Group>
                         
-                        <Form.Group as={Row} className="mb-3">
+                        <Form.Group as={ Row } className="mb-3">
                             <Col sm>
-                                <Form.Control maxLength={50} type="input" placeholder="Email Address" value={email} onChange={onChangeEmail} />
-                                {emailError && <div className="invalid-input">Please enter valid email format.</div>}
+                                <Form.Control maxLength={ 50 } type="input" placeholder="Email Address" name="userMail" value={ userMail } onChange={ onChangeEmail } />
+                                { emailError && <div className="invalid-input">Please enter valid email format.</div> }
                             </Col>
                         </Form.Group>
                         <br />
                         <div className="d-grid gap-1">
-                            <Button variant="secondary" onClick={onSubmit}>로그인</Button>
+                            <Button variant="secondary" onClick={ onSubmit }>가입</Button>
                         </div>
                     </Form>
                     <br />
--- router-member.js
+++ router-member.js
@@ -34,4 +34,27 @@ router.get('/login', (req, res) => {
 	//res.send({message: 'Hello Express!'});
 });
 
+//회원 저장
+router.post('/save', (req, res) => {
+	const { userId, userPw, userName, userMail } = req.body;
+	
+	//if(userId && userPw && userName && userMail){
+		 //return true;
+	//}else{
+	//	return(res.json({Message: "VALIDATE"}));
+		 //return false;
+	//}
+
+	const sql = `
+			INSERT INTO TBL_MEMBER (M_ID, M_PASS, M_NAME, M_MAIL, M_AUTH
+			) VALUES (?, ?, ?, ?, 1)
+		`
+	connection.query(sql, [userId, userPw, userName, userMail], (err, result) => {
+		if(err) return res.json({Message: "ERROR"});
+		return res.json(result);
+		//console.log("err", err);
+		//res.send(`/${id}`);
+	});
+});
+
 module.exports = router;

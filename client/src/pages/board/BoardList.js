69051aefd83765fe170474fa86c524b77d0d810b
PRESSRAIN-LEE <pressrainlee@gmail.com>
2023-10-09 17:44:41 +0900

    Pagination추가

--- client/src/pages/board/BoardList.js
+++ client/src/pages/board/BoardList.js
@@ -1,90 +1,227 @@
-import React, {useEffect, useState, Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from "axios";
+import queryString from 'query-string';
 
-import Board from '../../components/Board';
-import Pagination from '../../components/Pagination';
+//import Board from '../../components/Board';
+//import Pagination from '../../components/Pagination';
+//import Pagination from '../../components/Pagination1';
+import Pagination from 'react-js-pagination'
+//import ReactPaginate from 'react-paginate';
+
+//import Search from '../../components/Search1';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button } from 'react-bootstrap';
 
-const Boards = () => {
-//class Boards extends Component {	
-	//생성자
-	// constructor(props){
-	// 	super(props);
-	// 	this.state = {
-	// 		boards: '',
-	// 	  completed: 0
+function Boards(){
+	const navigate = useNavigate();
+	const [inputs, setInputs] = useState({
+		queryItem: "",
+		queryText: "",
+	});
+	const { queryItem, queryText } = inputs;
+	console.log("queryText: ", queryText);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			 axios.get(`/api/boards${window.location.search}`)
+			.then(res => {
+				if(res.data.Status === "SUCCESS"){
+					setData(res.data.Results[0]);
+					setBoardCount(res.data.Results[1][0].CNT);
+					setPageSize(res.data.pagination[0].pageSize);
+					setCurrentPage(Number(res.data.pagination[0].currentPage));
+					//setPage(Number(res.data.pagination[0].currentPage));
+					setPageBlock(res.data.pagination[0].pageBlock);
+					
+					//setBoardListNum(boardCount - (pageSize * (page - 1)));
+					//setBoardListNum(res.data.Results[1][0].CNT - (res.data.pagination[0].pageSize * (res.data.pagination[0].currentPage)));
+					setBoardListNum1(res.data.Results[1][0].CNT);
+					setBoardListNum2(res.data.pagination[0].pageSize)
+					setBoardListNum3(res.data.pagination[0].currentPage);
+
+					setTotalPageCount(res.data.pagination[0].totalPageCount);
+					//setPageNumbers(res.data.pagination[0].pageNumbers);
+
+					//setSearch()
+					//console.log("inputs: ", inputs);
+				}else{
+					//
+				}
+			})
+			.catch(err => console.log(err));
+		};
+		fetchData();
+	}, []);
+
+	const [data, setData] = useState([]);
+
+	//const [pagination, setPagination] = useState([]);			//pagination
+	const [pageSize, setPageSize] = useState(10);				//한페이지에 보여줄 리스트 row수(기본 10개) - router에서 설정
+	const [currentPage, setCurrentPage] = useState(1);			//현재 페이지 숫자
+	const [pageBlock, setPageBlock] = useState(10);				//한페이지에 보여줄 페이징 갯수(기본 10)
+	const [totalPageCount, setTotalPageCount] = useState(0);		//전체 페이지 수
+	const [boardCount, setBoardCount] = useState(0);				//전체게시 글 수
+	//const [pageNumbers, setPageNumbers] = useState(0);			//페이지 숫자들
+
+	//const [page, setPage] = useState(1);
+
+	//const [boardListNum, setBoardListNum] = useState();			//리스트 글번호
+	const [boardListNum1, setBoardListNum1] = useState();			//리스트 글번호
+	const [boardListNum2, setBoardListNum2] = useState();			//리스트 글번호
+	const [boardListNum3, setBoardListNum3] = useState();			//리스트 글번호
+	
+	//const [search, setSearch] = useState([]);			//리스트 검색
+
+	//const [queryItem, setQueryItem] = useState("");
+	//const [queryText, setQueryText] = useState("");
+	
+
+	// useEffect(() => {
+	// }, [currentPage]);
+
+	//if (boardCount <= 20) return; 
+
+	//console.log("currentPage1: ", currentPage)
+	// console.log("pageGroup: ", pageGroup);
+	// console.log("totalPage: ", totalPage);
+	// console.log("first: ", first);
+	// console.log("last: ", last);
+	// console.log("next: ", next);
+	// console.log("prev: ", prev);
+	// console.log("===================================");
+
+	// if (prev > 0) {
+	// 	console.log("A");
+	// }
+	//for (var i = first; i <= last; i++) {
+		//console.log("C"+i);
+	//}
+	// if (last < totalPage) {
+	// 	console.log("C");
+	// }
+
+	// const pagePrev = () => {
+	// 	<li className="page-item">
+	// 		<a className="page-link" href="#" aria-label="Previous">
+	// 			<span aria-hidden="true">&laquo;</span>
+	// 		</a>
+	// 	</li>
+	// };
+	// const pageNumber = () => {
+	// 	const result = [];
+	// 	// for (var i = 0; i < first; i++) {
+	// 	// 	result.push(
+	// 	// 		<li className="page-item" key={i}>
+	// 	// 			<a className="page-link" href="#" aria-label="Previous">
+	// 	// 				<span aria-hidden="true">&laquo;</span>
+	// 	// 			</a>
+	// 	// 		</li>
+	// 	// 	);
+	// 	// }
+	// 	for (var i = first; i <= last; i++) {
+	// 		result.push(
+	// 			<li className="page-item" key={i}>
+	// 				<a href="#" className="page-link">{i}</a>
+	// 			</li>
+	// 		);
+			
 	// 	}
-	//   }
-	//   stateRefresh = () => {
-	// 	//state를 초기화 시킴
-	// 	this.setState({
-	// 		boards: '',
-	// 	  completed: 0
-	// 	});
-	// 	this.callApi()
-	// 	  .then(res => this.setState({boards: res}))
-	// 	  .catch(err => console.log(err));
-	//   }
+
+	// 	// for (var i = last; i <= last; i++) {
+	// 	// 	result.push(
+	// 	// 		<li className="page-item" key={i}>
+	// 	// 			<a className="page-link" href="#" aria-label="Next">
+	// 	// 				<span aria-hidden="true">&raquo;</span>
+	// 	// 			</a>
+	// 	// 		</li>
+	// 	// 	);
+	// 	// }
+	// 	return result;
+	// };
+
+	// useEffect(() => {
+	// 	pageNumber()
+	// }, []);
+
+	// const pageNext = () => {
+	// 	//
+	// };
 	
-	//   componentDidMount() {
-	// 	this.callApi()
-	// 	  .then(res => this.setState({boards: res}))
-	// 	  .catch(err => console.log(err));
-	//   }
-	
-	//   callApi = async() => {
-	// 	const response = await fetch('/api/boards');
-	// 	const body = await response.json();
-	// 	//console.log(body);
-	// 	return body;
-	//   }
-
-	const [boards, setBoards] = useState([]);
-
-	//페이징
-	const [currentPage, setCurrentPage] = useState(1);
-	const [boardPerPage] = useState(5);
-
-	const [page, setPage] = useState(1); //페이지
-	const limit = 10; // posts가 보일 최대한의 갯수
-	const offset = (page-1)*limit; // 시작점과 끝점을 구하는 offset
+	const queryObj = queryString.parse(window.location.search);
+	const { id, page } = queryObj;
+	console.log("id: ", id);
 
 	// const loadData = async () => {
 	// 	const response = await axios.get('/api/boards');
 	// 	setBoards(response.data);
 	// };
 
-	// useEffect(() => {
-	// 	loadData();
-	// }, []);
+	const onChangeInput = (e) =>{
+		e.preventDefault();
+		const { value, name } = e.target;
+		// const nextInputs = {
+		// 	...inputs,
+		// 	[name]: value,
+		// };
+		// setInputs(nextInputs);
+		setInputs((inputs) => {
+			return {...inputs, [name]: value};
+		});
+	};
 
-	useEffect(() => {
-		fetch('/api/boards')
-			.then(res => res.json())
-			.then(data => {
-				//console.log(1, data);
-				setBoards(data);
-			})
-			.catch(err => console.log(1, err));
-		}, []);
+	const onSearch = () =>{
+		//console.log("queryText: ", queryText);
+		window.location.href=`/board?queryItem=${queryItem}&queryText=${queryText}`;
+	}
 
-	//페이징
-	const indexOfLast = currentPage * boardPerPage;
-	const indexOfFirst = indexOfLast - boardPerPage;
-	const currentBoards = boards.slice(indexOfFirst, indexOfLast);
-	const paginate = (pageNumber) => setCurrentPage(pageNumber);
-	
-	//render() {
+	const goView = (id) => {
+		//window.location.href=`/hit?id=${id}`
+		console.log("ID: ", id);
+		axios.get("/api/boards/hit/" + id)
+		.then(res => {
+			console.log(res);
+			//navigate('/board');
+			if(res.data.Status === true){
+				navigate(`/board/detail/${id}`);
+			}
+		})
+		.catch(err => console.log(err));
+	}
+	const goEdit = () => {}
+
+	const pageClick = (page) => {
+		setCurrentPage(page);
+		//setCurrentPage((page) => Number(page)+1);
+		window.location.href=`/board?page=${page}`
+	}
+
+	//const handlePageChange = (page) => { setCurrentPage(page); };
+
 	return (
 	  <div className="justify-content-center align-items-center">
 	  	<div>
 		<h1>목록</h1>
 		
+		{/* //https://onethejay.tistory.com/196 */}
+
+		{/* <Search/> */}
+		<div>
+			<Form.Select name="queryItem" id="queryItem" onChange={ onChangeInput } className="mb-3">
+				<option value="">-선택-</option>
+				<option value="title">제목</option>
+				<option value="contents">내용</option>
+			</Form.Select>
+			<Form.Control className="form-inline" type="text" placeholder="Normal text" name="queryText" value={queryText} id="queryText" onChange={ onChangeInput } />
+			<Button variant="primary" onClick={onSearch} >검색</Button>
+		</div>
+
+{queryText}
+
 		<div className="row">
 			<div className="col">
-				총 100건 1/20페이지
+				총 {boardCount}건 {currentPage} / {totalPageCount}페이지
 			</div>
 		</div>
 		<table className="table table-striped table-hover">
@@ -100,44 +237,150 @@ const Boards = () => {
 			</thead>
 			<tbody>
 			{
-				// this.state.boards ? this.state.boards.map(b => {
-				// 	return(
-				// 		<Board
-				// 			stateRefresh={this.stateRefresh}
-				// 			key = {b.id}    //map을 사용하면 key값은 필수(unique)
-				// 			id = {b.id}
-				// 			name = {b.member_name}
-				// 			title = {b.board_title}
-				// 			regDate = {b.created_at}
-				// 			ref_step = {b.ref_step}
-				// 		/>
-				// 		);
-				// 	})
-				// : "&nbsp;"
-			}
-
-			{
-				boards.map(b => (
-					<Board
-						key = {b.id}    //map을 사용하면 key값은 필수(unique)
-						id = {b.id}
-						name = {b.member_name}
-						title = {b.board_title}
-						regDate = {b.created_at}
-						read = {b.board_read}
-						ref_level = {b.ref_level}
-						ref_step = {b.ref_step}
-						stateRefresh={boards}
-					/>
-				))
-			}
+				data.map((board, index) => {
+					return <tr key={index}>
+						<td>{boardListNum1 - (boardListNum2 * (boardListNum3 - 1)) - index}</td>
+						<td>
+							{
+								board.ref_step > 0 ?
+									[...Array(parseInt(board.ref_level))].map((n, index) => {
+										return <span style={{ whiteSpace: 'pre-wrap'}} key={index}> { " " } </span>
+									})
+								
+									: ""
+							}
+							{/* <Link to={`/board/detail/${props.id}`}> */}
+							<Link onClick={() => goView(board.id) } to="">
+							{/* <Link onClick={ addHit } to={`/board/detail/${ board.id }`}></Link> */}
+								{
+									board.ref_level > 0 ? '답변: ' + board.board_title : board.board_title
+								}
+							</Link>
+						</td>
+						<td>{ board.M_NAME }</td>
+						<td>{ board.created_at }</td>
+						<td>{ board.board_read }</td>
+						<td>
+							<Button size="sm" type="button" className="btn mb-2 mx-1" variant="success" onClick={ goEdit }>수정</Button>
+							{/* <BoardBtnDel 
+								stateRefresh={board.stateRefresh} 
+								id={board.id}
+							/> */}
+						</td>
+					</tr>
+				})}
 			</tbody>
 		</table>
 
-		{/* <Pagination /> */}
-		<Pagination boardsPerPage={boardPerPage} totalBoards={boards.length} currentPage={currentPage} paginate={paginate}></Pagination>
-		{/* <Pagination boardsPerPage={boardPerPage} totalBoards={boards.length} paginate={setCurrentPage}></Pagination> */}
-		{/* <Pagination limit={limit} page={page} totalBoards={boards.length} setPage={setPage}/> */}
+		{/* <ReactPaginate
+			previousLabel={"previous"}
+			nextLabel={"next"}
+			breakLabel={"..."}
+			pageCount={25}
+			marginPagesDisplayed={2}
+			pageRangeDisplayed={6}
+			onPageChange={handlePageClick}
+			containerClassName={'pagination justify-content-center'}
+			pageClassName={'page-item'}
+			pageLinkClassName={'page-link'}
+			previousClassName={'page-item'}
+			previousLinkClassName={'page-link'}
+			nextClassName={'page-item'}
+			nextLinkClassName={'page-link'}
+			breakLinkClassName={'page-link'}
+			activeClassName={'active'}
+			//renderOnZeroPageCount={null}
+		/> */}
+
+		{/* 페이징 */}
+		<nav aria-label="Page navigation example">
+			<ul className="pagination justify-content-center">
+				{/* <li className="page-item">
+					<a className="page-link" href="#" aria-label="Previous">
+						<span aria-hidden="true">&laquo;</span>
+					</a>
+				</li> */}
+				{
+					// numbers.map((n, i) => (
+					// 	<li className={`page-item  ${currentPage === (n+1) ? "active" : ""}`} key={i} >
+					// 		<a href="#" className="page-link">{n}</a>
+					// 	</li>
+					// ))
+				}
+
+				{/* <li className="page-item">
+					<a className="page-link" href="#" aria-label="Next">
+						<span aria-hidden="true">&raquo;</span>
+					</a>
+				</li> */}
+
+				{/* {pageNumber()} */}
+
+				{/* {pageNumbers.map(number => (
+					<li key={number} className="page-item"> */}
+						{/* <a onClick={() => paginate(number)} className="page-link" style={currentPage == number ? {color: '#17a2b8'} : null}>
+							{number}
+						</a> */}
+					{/* </li>
+				))}
+			 */}
+				{/* <li className="page-item">
+					<a className="page-link" href="#" aria-label="Previous">
+					<span aria-hidden="true">&laquo;</span>
+					</a>
+				</li>
+				<li className="page-item"><a className="page-link" href="/board?page=1">1</a></li>
+				<li className="page-item"><a className="page-link" href="/board?page=2">2</a></li>
+				<li className="page-item"><a className="page-link" href="/board?page=3">3</a></li> */}
+				{/* <li className="page-item">
+				<a className="page-link" href="#" aria-label="Next">
+					<span aria-hidden="true">&raquo;</span>
+				</a>
+				</li> */}
+			</ul>
+		</nav>
+
+		<Pagination
+          activePage={currentPage}
+          //activePage={page}
+          itemsCountPerPage={pageSize}
+          totalItemsCount={boardCount}
+          pageRangeDisplayed={pageBlock}
+		  activeLinkClass = {"active"}
+		  activeClass = {"active"}
+		  innerClass = {"pagination justify-content-center"}
+		  itemClass = {"page-item"}
+		  //itemClassFirst = {"page-link"}
+		  //itemClassPrev = {"page-link"}
+		  //itemClassLast = {"page-link"}
+		  //itemClassNext = {"page-link"}
+		  disabledClass = {"disabled"}
+		  linkClass = {"page-link"}
+		  linkClassFirst = {"page-link"}
+		  linkClassPrev = {"page-link"}
+		  linkClassNext = {"page-link"}
+		  linkClassLast = {"page-link"}
+		  firstPageText = {"처음"}
+		  prevPageText = {"이전"}
+		  nextPageText = {"다음"}
+		  lastPageText = {"마지막"}
+		  hideFirstLastPages = {false}
+          onChange={pageClick}>
+          {/* onChange={handlePageChange} */}
+        </Pagination>
+
+		{/* 페이징: <Pagination
+        pageBlock={pageBlock}
+		page={currentPage}
+		pageSize = {pageSize}
+		boardCount = {boardCount}
+		totalPageCount = {totalPageCount}
+		
+		// totalBoards={boards.length}
+		// pageClick={pageClick}
+
+		//page_size={boardPerPage}
+      ></Pagination> */}
 
 		<div className="d-flex justify-content-end">
 			<Link className="btn btn-primary" to="/board/create" role="button">글쓰기</Link>
@@ -145,7 +388,6 @@ const Boards = () => {
 		</div>
 	  </div>
 	)
-	//}
 }
 
 export default Boards;
--- router-board.js
+++ router-board.js
@@ -1,10 +1,16 @@
-const fs = require('fs');
+//const fs = require('fs');
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const path = require('path');
+
+const uploadDir = path.join(__dirname, '../../upload/board');
+var mime = require('mime');
+const fs = require('fs');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,48 +34,125 @@ router.get('/', (req, res) => {
 	//res.send({message: 'Hello Members!'});
 	//console.log(req.body);
 
-	const { page, page_size } = req.query;
-	let start_limit = 0;
-	let end_limit = page_size;
-	if (page === "" || page === undefined || typeof page === "undefined" || page === null) {
-		//const start_limit = 0 * page_size; // 1페이지는 무조건 0부터 시작
+	//const queryObj = queryString.parse(window.location.search);
+	//const { queryText } = queryObj;
+	//console.log("queryText: ", queryText);
+	//console.log("location.search:", location.search);
+
+	// const { page, page_size } = req.query;
+	// let start_limit = 0;	// 1페이지는 무조건 0부터 시작
+	// let end_limit = page_size;
+	// if (page === "" || page === undefined || typeof page === "undefined" || page === null) {
+	// 	//const start_limit = 0 * page_size;
+	// }else{
+	// 	start_limit = (page - 1) * page_size;
+	// }
+	
+	// console.log("list-(start_limit, page_size)", start_limit, ", ", end_limit);
+
+	let {queryItem, queryText, page} = req.query;
+	if(!queryItem || queryItem === "undefined"){
+		searchItem = "";
 	}else{
-		start_limit = (page - 1) * page_size; // 1페이지는 무조건 0부터 시작
+		searchItem = queryItem ? queryItem : "";
+	}
+	if(!queryText || queryText === "undefined"){
+		searchText = "";
+	}else{
+		searchText = queryText ? queryText : "";
+	}
+	currentPage = page ? page : 1;
+
+	console.log("searchItem: ", searchItem);
+	console.log("searchText: ", searchText);
+
+	const pageSize = 5;	//한페이지에 보여줄 리스트 row수
+	const pageBlock = 5;	//한페이지에 보여줄 페이징 갯수
+	let startLimit = 0;
+	if (currentPage === "" || currentPage === undefined || typeof currentPage === "undefined" || currentPage === null) {
+		startLimit = 0 * pageSize; 	// 1페이지는 무조건 0부터 시작
+	}else{
+		startLimit = (currentPage - 1) * pageSize; // 1페이지는 무조건 0부터 시작
+	}
+	console.log("req.query : ", req.query);
+
+	let subSql = "";
+	switch (searchItem){
+		case "title":
+			subSql += "AND INSTR(board_title, ?) > 0 ";
+			break;
+		case "contents":
+			subSql += "AND INSTR(board_content, ?) > 0 ";
+			break;
+		default:
+			subSql += "AND (INSTR(board_title, ?) > 0 ";
+			subSql += "		OR INSTR(board_content, ?) > 0) ";
 	}
 	
-	console.log("list-(start_limit, page_size)", start_limit, ", ", end_limit);
+	let sql = "";
+		sql += "SELECT id, ref_level, ref_step, board_title, board_read, board_file1, board_file2, DATE_FORMAT(created_at, '%Y-%m-%d') AS created_at";
+		sql += ", M_ID, M_NAME ";
+		sql += "	FROM TBL_BOARD A INNER JOIN TBL_MEMBER B ON A.member_seq = B.M_SEQ "
+		sql += "	WHERE 1 = 1 ";
+		sql += "	AND board_state = 'Y' ";
+		//sql += "	AND INSTR(board_title, ?) > 0 "
+		sql += 		subSql;
+		sql += "	ORDER BY ref DESC, ref_level ASC, id DESC ";
+		sql += "	LIMIT ?, ? ";
 
-	const sql = `
-			SELECT id, member_name, board_title, board_read, DATE_FORMAT(created_at, '%Y-%m-%d') AS created_at 
-			, ref, ref_level, ref_step
-			FROM TBL_BOARD 
-			WHERE 1 = 1
-			AND board_state = 'Y' 
-			ORDER BY ref DESC, ref_level ASC, id DESC
-			LIMIT ${start_limit}, ${end_limit}
-			;
-		`;
-	db.query(sql, (err, rows, fields) => {	//rows에 디비내용을 저장
-			if(err){
-				console.log("DB 에러");
-				//console.log(err);
-			}else{
-				//console.log(rows);
-				res.send(rows);
+	let sql2 = "; ";
+		sql2 += "SELECT COUNT(*) CNT ";
+		sql2 += "FROM TBL_BOARD A INNER JOIN TBL_MEMBER B ON A.member_seq = B.M_SEQ  ";
+		sql2 += "WHERE 1 = 1 ";
+		sql2 += "AND board_state = 'Y' ";
+		//sql2 += "AND INSTR(board_title, ?) > 0 ";
+		sql2 += subSql;
+
+		let sqlParams = [];
+		if(searchItem){
+			sqlParams = [searchText, startLimit, pageSize, searchText];
+		}else{
+			sqlParams = [searchText, searchText, startLimit, pageSize, searchText, searchText];
+		}
+
+		db.query(sql + sql2, sqlParams, (err, rows, fields) => {	//rows에 디비내용을 저장
+			const totalPageCount = Math.ceil(rows[1][0].CNT/pageSize);
+			let pageNumbers = [];
+			for (var i=0; i< totalPageCount; i++){
+			//for (var data of totalPageCount){
+				pageNumbers.push(i);
 			};
+			//console.log("A: ", pageNumbers);
+
+			if(err) return res.json({Error: "SQL QUERY ERROR"});
+			return res.json({
+				Status: "SUCCESS", 
+				Results: rows,
+				pagination: [{
+					pageSize: pageSize,
+					pageBlock: pageBlock,
+					totalPageCount: Number(totalPageCount),
+					currentPage: currentPage,
+					pageBlock: pageBlock,
+					pageNumbers: pageNumbers,
+				}],
+				Search: [{
+					queryItem: searchItem,
+					queryText: searchText,
+				}]
+			});
+			// if(err){
+			// 	console.log("DB 에러");
+			// 	//console.log(err);
+			// }else{
+			// 	//console.log(rows);
+			// 	res.send(rows);
+			// };
 		}
 	);
-
-	/*
-	const sql = "SELECT * FROM CUSTOMER WHERE isDeleted = 0";
-	db.query(sql, (err, rows, fields) => {
-		console.log("err", err);
-		console.log("rows", rows);
-		console.log("fields", fields);
-	});*/
 });
 
-//글 카운트
+//글 카운트 - 사용안함
 router.get('/count', (req, res) => {
 	const sql = `
 		SELECT COUNT(*) TOTAL_COUNT
@@ -77,14 +160,18 @@ router.get('/count', (req, res) => {
 		WHERE 1 = 1 
 		AND board_state = 'Y' 
 		`;
-		db.query(sql, (err, rows, fields) => {	//rows에 디비내용을 저장
-		if(err){
-			console.log("DB연결 실패");
-			// console.log(err);
-		}else{
-			//console.log("rows: ", rows);
-			res.send(rows);
-		};
+		db.query(sql, (err, result, fields) => {
+			if(err) return res.json({Error: "SQL QUERY ERROR"});
+			return res.json(result);
+			//console.log("result: ", result);
+			
+		// if(err){
+		// 	console.log("DB연결 실패");
+		// 	// console.log(err);
+		// }else{
+		// 	//console.log("rows: ", rows);
+		// 	res.send(rows);
+		// };
 	});
 });
 
@@ -137,16 +224,22 @@ router.post('/save', (req, res) => {
 });
 
 //조횟수 증가
- router.put('/hit/:idx', (req, res) => {
+ router.get('/hit/:idx', (req, res) => {
+	//console.log(1, "id=====================");
+	//res.render("./pages/board/BoardList");
  	const id = req.params.idx;
+	//console.log(1, id);
 	const up_sql = `UPDATE TBL_BOARD SET 
 				board_read = board_read + 1 
 				WHERE id = ? `;
-	//console.log(1, up_sql);
+	//console.log(1, id);
 	db.query(up_sql, [id], (err, result) => {
-		//if(err) return res.json({Message: "Error!!"});
+		if(err) return res.json({Message: "Error!!"});
 		//return res.json(result);
-		//res.send(`/${id}`);
+		//res.send(`/board/detail/${id}`);
+		//navigate('/board');
+		res.send({Status: true, data: result});
+		//console.log(2, result);
 	});
  });
 
@@ -154,22 +247,79 @@ router.post('/save', (req, res) => {
 router.get('/detail/:idx', (req, res) => {
 	const id = req.params.idx;
 	 const sql = `
-			SELECT * 
+			SELECT *, DATE_FORMAT(created_at, '%Y-%m-%d %H:%i:%s') AS created_at 
 			FROM TBL_BOARD 
 			WHERE 1 = 1 
 			AND id = ?
 			`;
 	 db.query(sql, [id], (err, rows, fields) => {	//rows에 디비내용을 저장
-	 		if(err){
-	 			console.log("DB 실패");
-	 			// console.log(err);
-	 		}else{
-	 			//console.log(rows);
-	 			res.send(rows);
-	 		};
+		if(err) return res.json({Message: "Error!!"});
+		res.send({Status: true, results: rows});
+	 		// if(err){
+	 		// 	console.log("DB 실패");
+	 		// 	// console.log(err);
+	 		// }else{
+	 		// 	//console.log(rows);
+	 		// 	res.send(rows);
+	 		// };
 	 	}
 	 );
 });
+
+//첨부파일 다운로드
+router.get('/attach/:id/:order', (req, res) => {
+	const { id, order } = req.params;
+	const upload_folder = 'upload/board/';
+	var getDownloadFilename = require('./lib/getDownloadFilename').getDownloadFilename;
+	
+	//테이블에서 seq로 파일 검색
+	const sql = `SELECT board_file1, board_file1_ori, board_file2, board_file2_ori FROM TBL_BOARD WHERE id = ? `
+	db.query(sql, [id], (err, results) => {
+		//if(err) throw err;
+		try {
+			let file1 = upload_folder + results[0].board_file1;
+			let file1Ori = upload_folder + results[0].board_file1_ori;
+			let file2 = upload_folder + results[0].board_file2;
+			let file2Ori = upload_folder + results[0].board_file2_ori;
+
+			let file = "";
+			let fileOri = "";
+			switch(order){
+				case "1":
+					file = file1;
+					fileOri = file1Ori;
+					break;
+				case "2":
+					file = file2;
+					fileOri = file2Ori;
+					break;
+			}
+			if (fs.existsSync(file)) { // 파일이 존재하는지 체크
+				var filename = path.basename(fileOri);	// 파일 경로에서 파일명(확장자포함)만 추출
+				var mimetype = mime.getType(file);		// 파일의 타입(형식)을 가져옴
+
+				//res.setHeader('Content-disposition', 'attachment; filename=' + filename1Ori); // 다운받아질 파일명 설정
+				res.setHeader('Content-disposition', 'attachment; filename=' + getDownloadFilename(req, filename)); // 다운받아질 파일명 설정
+				res.setHeader('Content-type', mimetype); // 파일 형식 지정
+
+				var filestream = fs.createReadStream(file);
+				filestream.pipe(res);
+			}else{
+				res.send('해당 파일이 없습니다.');  
+				return;
+			}
+		}catch(e){
+			console.log(e);
+			res.send('파일을 다운로드하는 중에 에러가 발생하였습니다.');
+			return;
+		}
+	});
+});
+
+//첨부파일 삭제
+router.get('/fileDelete/:id/:order', (req, res) => {
+});
+
 
 //수정페이지 이동
 router.get('/edit/:idx', (req, res) => {
@@ -297,7 +447,7 @@ router.post('/replySave/:idx', (req, res) => {
 
 //삭제
 //참고 동영상: https://www.youtube.com/watch?v=y5NvOade3sk
-router.put('/delete/:idx', (req, res) => {
+router.get('/delete/:idx', (req, res) => {
 	const id = req.params.idx;
 	//console.log(id);
 
@@ -306,7 +456,8 @@ router.put('/delete/:idx', (req, res) => {
 				WHERE id = ? `;
 		db.query(up_sql, [id], (err, result) => {
 		if(err) return res.json({Message: "ERROR"});
-		return res.json(result);
+		res.send({Status: true, results: result});
+		//return res.json(result);
 		//console.log("result : " + (err));
 	});
 });
